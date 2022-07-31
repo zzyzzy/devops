cd7c17c520f141ea284def821678e6929e2e5b06
dedvops <zzyzigy@devops.co.kr>
2022-07-31 10:20:05 +0900

    initial commit

---
+++ src/test/java/devops/zzyzzy/hellodevops/HelloDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package devops.zzyzzy.hellodevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
